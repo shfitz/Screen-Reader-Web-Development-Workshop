996ce42e907ab2dccd9874109d28c190e8546195
Claire K-V <ckv214@nyu.edu>
2018-01-16 10:25:56 +0300

    day 5 stuffs

---
+++ Day 5/Javascript_Example_2/script.js
@@ -0,0 +1,5 @@
+function addWord(){
+var word = document.getElementById("input_text").value;
+document.getElementById("input-word").innerHTML = "You just wrote "+ word + " in the input!";
+console.log("boop");
+}
